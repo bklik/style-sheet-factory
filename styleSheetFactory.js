397352ed59bf7e869e462d4286b81f254835ba70
Brenton Klik <brenton.klik@gmail.com>
2016-02-08 10:58:51 -0500

    Update styleSheetFactory.js
    Minor refactoring. Added some returns.

--- styleSheetFactory.js
+++ styleSheetFactory.js
@@ -12,15 +12,46 @@ style sheet.
 /**********************************************************************/
 angular.module('style-sheet-factory', [])
 
-.factory('styleSheetFactory', ['$log', function($log){
+.factory('styleSheetFactory', ['$log', function($log) {
+    /************************************************************************
+    Local Variables
+    ************************************************************************/
+    // Array of selectors modified by the browser.
+    var _modifiedSelectors = [];
+
+    var _insertCSSRule = function(sheet, selector, rules, index) {
+        index = index || 1;
+
+        try {
+            sheet.insertRule(selector + "{" + rules + "}", index);
+
+            // Parse browser's selector
+            var newSelector = sheet.cssRules[index].cssText.split(' {')[0];
+
+            // If browser modified the selector, store it.
+            if(selector !== newSelector) {
+                var selectorObj = {
+                    'old': selector,
+                    'new': newSelector
+                };
+
+                _modifiedSelectors.push(selectorObj);
+            }
+
+            return sheet.cssRules[1].cssText;
+        } catch(e) {
+            $log.error('Failed to add rule: ' + selector);
+        }
+    };
+
     return {
         // Finds and returns the browsers's main style sheet.
         getStyleSheet: function() {
             for(var i=0; i<document.styleSheets.length; i++) {
                 if(
-                    document.styleSheets[i].media.mediaText == '' ||
-                    document.styleSheets[i].media.mediaText == 'all' ||
-                    document.styleSheets[i].media.mediaText == 'screen'
+                    document.styleSheets[i].media.mediaText === '' ||
+                    document.styleSheets[i].media.mediaText === 'all' ||
+                    document.styleSheets[i].media.mediaText === 'screen'
                 ) {
                     return document.styleSheets[i];
                 }
@@ -33,7 +64,9 @@ angular.module('style-sheet-factory', [])
         // CSS for non-standardized properties.
         getPrefix: function() {
             var prefixes = ['Webkit', 'Moz', 'ms', 'O', 'Khtml'];
-            for(var i=0; i<prefixes.length; i++) {
+            var len = prefixes.length;
+
+            for(var i=0; i<len; i++) {
                 if(document.body.style[ prefixes[i] + 'AnimationName' ] !== undefined) {
                     return '-'+prefixes[i].toLowerCase()+'-';
                 }
@@ -44,8 +77,33 @@ angular.module('style-sheet-factory', [])
         // Returns whether a rule of that selector exists in the stylesheet.
         hasCSSRule: function(sheet, selector) {
             var rules = sheet.cssRules;
-            for(var i=0; i<rules.length; i++) {
-                if(rules[i].selectorText == selector) {
+            var len = _modifiedSelectors.length
+
+            // Check for a modified selector.
+            for(var m=0; m<len; m++) {
+                if(selector === _modifiedSelectors[m].old) {
+                    selector = _modifiedSelectors[m].new;
+                }
+            }
+
+            len = rules.length;
+
+            for(var i=0; i<len; i++) {
+                if(rules[i].selectorText === selector) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
+        // Returns whether a keyframe of that name exists in the stylesheet.
+        hasCSSKeyframe: function(sheet, name) {
+            var rules = sheet.cssRules;
+            var len = rules.length;
+
+            for(var i=0; i<len; i++) {
+                if(rules[i].name === name) {
                     return true;
                 }
             }
@@ -55,45 +113,57 @@ angular.module('style-sheet-factory', [])
     
         // If no selector of that rule exists, adds the new rule to the stylesheet.
         addCSSRule: function(sheet, selector, rules, index) {
-            index = index || 1;
-
             if(!this.hasCSSRule(sheet, selector)) {
-                try {
-                    sheet.insertRule(selector + "{" + rules + "}", index);
-
-                    // Compare selectors
-                    var newSelector = sheet.cssRules[index].cssText.split(' {')[0];
-
-                    if(selector !== newSelector) {
-                        $log.warn(
-                            'The browser has changed the selector of the rule you added to the StyleSheet.' +
-                            '\nOld: '+selector+
-                            '\nNew: '+newSelector+
-                            '\nThis may cause duplicate rules and harm browser performance. Replace your ' +
-                            'selector with the new selector.'
-                        );
-                    }
-                } catch(e) {
-                    $log.error('Failed to add rule: ' + selector);
-                }
+                return _insertCSSRule(sheet, selector, rules, index);
             }
         },
     
-        // Removes a rule of the existing selector from the stylesheet.
+        // Removes a rule of the given selector from the stylesheet.
         removeCSSRule: function(sheet, selector) {
             var rules = sheet.cssRules;
-            for(var i=0; i<rules.length; i++) {
-                if(rules[i].selectorText == selector) {
-                    sheet.deleteRule(i);
-                    break;
+            var len = _modifiedSelectors.length;
+
+            // Check for a modified selector and remove it.
+            for(var m=0; m<len; m++) {
+                if(selector === _modifiedSelectors[m].old) {
+                    selector = _modifiedSelectors[m].new;
+
+                    _modifiedSelectors.splice(m, 1);
                 }
             }
+
+            len = rules.length;
+
+            for(var i=0; i<len; i++) {
+                if(rules[i].selectorText === selector) {
+                    sheet.deleteRule(i);
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
+        // Removes a keyframe of the given name from the stylesheet.
+        removeCSSKeyframe: function(sheet, name) {
+            var rules = sheet.cssRules;
+            var len = rules.length;
+
+            for(var i=0; i<len; i++) {
+                if(rules[i].name === name) {
+                    sheet.deleteRule(i);
+                    return true;
+                }
+            }
+
+            return false;
         },
     
         // Adds a keyframes animation to the stylesheet with te appropriate prefixing.
         addCSSKeyframes: function(sheet, name, rules, index) {
-            //sheet.insertRule('@'+this.getPrefix()+'keyframes ' + name + "{" + rules + "}", index);
-            this.addCSSRule(sheet, '@'+this.getPrefix()+'keyframes '+name, rules, index);
+            if(!this.hasCSSKeyframe(sheet, name)) {
+                return _insertCSSRule(sheet, '@'+this.getPrefix()+'keyframes '+name, rules, index);
+            }
         }
     }
 }]);
